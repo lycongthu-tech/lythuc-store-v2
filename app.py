ed1897dee4649f951d25b34c17a33e31993f3b36
lycongthuc <lycongthu@gmail.com>
2026-09-22 14:22:30 +0700

    f

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def init_db():
     for slug, name in DEFAULT_CATEGORIES:
         if DATABASE_URL:
             cursor.execute(
-                'INSERT INTO categories (name, slug) VALUES (?, ?) ON CONFLICT (slug) DO NOTHING',
+                'INSERT INTO categories (name, slug) VALUES (%s, %s) ON CONFLICT (slug) DO NOTHING',
                 (name, slug)
             )
         else:
@@ -321,10 +321,14 @@ def init_db():
             ("Thảm Tập Yoga Cao Su Non Chống Trượt", 140000, 200000, "https://images.unsplash.com/photo-1601925260368-ae2f83cf8b7f", "https://vt.tiktok.com/", "phu-kien", "-30%"),
             ("Bột Whey Protein Hỗ Trợ Tăng Cơ Giảm Mỡ", 650000, 850000, "https://images.unsplash.com/photo-1579722883378-7634e4096053", "https://vt.tiktok.com/", "whey-tpbs", "-23%")
         ]
-        conn.executemany('''
+        insert_product_query = '''
+            INSERT INTO san_pham (ten, gia, gia_cu, anh, link_affiliate, danh_muc, tag)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ''' if DATABASE_URL else '''
             INSERT INTO san_pham (ten, gia, gia_cu, anh, link_affiliate, danh_muc, tag)
             VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', san_pham_mau)
+        '''
+        conn.executemany(insert_product_query, san_pham_mau)
         conn.commit()
 
     conn.close()
